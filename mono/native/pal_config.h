624f69143d3a82dedc3296a0d293cd171a69cc99
Zoltan Varga <vargaz@gmail.com>
2019-01-21 17:11:11 -0500

    [runtime] Add pal_runtimeinformation.c to Mono.Native, required by the netcore Environment class.

--- mono/native/pal_config.h
+++ mono/native/pal_config.h
@@ -11,3 +11,22 @@
 /* arc4random_buf() is not available even when configure seems to find it */
 #undef HAVE_ARC4RANDOM_BUF
 #endif
+
+/* Based on src/Native/Unix/configure.cmake */
+#if defined(TARGET_OSX)
+#define PAL_UNIX_NAME "OSX"
+#elif defined(__linux__)
+#define PAL_UNIX_NAME "LINUX"
+#else
+#error "Cannot define PAL_UNIX_NAME"
+#endif
+
+#if defined(TARGET_ARM)
+#define _ARM_ 1
+#elif defined(TARGET_ARM64)
+#define _ARM64_ 1
+#elif defined(TARGET_AMD64)
+#define _AMD64_ 1
+#elif defined(TARGET_X86)
+#define _X86_ 1
+#endif
